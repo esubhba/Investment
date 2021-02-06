741ed6d1be8c8a0f08a51cfbed56c5b5a56c3f51
SU20126531 <subhadeep.baladhikari@wipro.com>
2021-02-06 23:04:08 +0530

    Add Feign settings

--- mangement/src/main/java/com/manage/service/mangement/MangementApplication.java
+++ mangement/src/main/java/com/manage/service/mangement/MangementApplication.java
@@ -2,8 +2,12 @@ package com.manage.service.mangement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MangementApplication {
 
 	public static void main(String[] args) {
