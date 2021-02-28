cd8a2e161f7f610df8f14702f304905bcf3706c6
SU20126531 <subhadeep.baladhikari@wipro.com>
2021-02-28 22:53:24 +0530

    Change in Filter

--- Gateway/src/main/java/com/za1975/Gateway/config/FilterConfiguration.java
+++ Gateway/src/main/java/com/za1975/Gateway/config/FilterConfiguration.java
@@ -11,8 +11,7 @@ import reactor.core.publisher.Mono;
 @Log4j2
 public class FilterConfiguration {
 
-	@Bean
-	
+	@Bean	
 	public GlobalFilter firstFilter()
 	{
 		return (exchange,chain)->
@@ -22,17 +21,17 @@ public class FilterConfiguration {
 			return chain.filter(exchange).then(Mono.fromRunnable(()->{
 				log.info("################################ Post Filter Executed ##################################");
 				
-				exchange.getResponse().getHeaders().add("Access-Control-Allow-Origin", "*");
-		        exchange.getResponse().getHeaders().add("X-Frame-Options", "SAMEORIGIN");
-		        exchange.getResponse().getHeaders().add("X-Xss-Protection", "1; mode=block");
-		        exchange.getResponse().getHeaders().add("X-Content-Type-Options", "nosniff");
-		        exchange.getResponse().getHeaders().add("Content-Security-Policy", "block-all-mixed-content; base-uri 'self'; frame-ancestors 'self'; form-action 'self'; object-src 'none';");
-		        exchange.getResponse().getHeaders().add("Referrer-Policy", "no-referrer-when-downgrade");
-		        exchange.getResponse().getHeaders().add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-		        exchange.getResponse().getHeaders().add("Feature-Policy", "microphone 'none'; geolocation 'none'");
-		        exchange.getResponse().getHeaders().add("X-Permitted-Cross-Domain-Policies", "none");
-		        exchange.getResponse().getHeaders().add("Expect-CT", "max-age=86400, enforce");
-		        exchange.getResponse().getHeaders().add("Access-Control-Allow-Methods", "POST, GET, OPTIONS,PATCH,PUT,DELETE");
+//				exchange.getResponse().getHeaders().add("Access-Control-Allow-Origin", "*");
+//		        exchange.getResponse().getHeaders().add("X-Frame-Options", "SAMEORIGIN");
+//		        exchange.getResponse().getHeaders().add("X-Xss-Protection", "1; mode=block");
+//		        exchange.getResponse().getHeaders().add("X-Content-Type-Options", "nosniff");
+//		        exchange.getResponse().getHeaders().add("Content-Security-Policy", "block-all-mixed-content; base-uri 'self'; frame-ancestors 'self'; form-action 'self'; object-src 'none';");
+//		        exchange.getResponse().getHeaders().add("Referrer-Policy", "no-referrer-when-downgrade");
+//		        exchange.getResponse().getHeaders().add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+//		        exchange.getResponse().getHeaders().add("Feature-Policy", "microphone 'none'; geolocation 'none'");
+//		        exchange.getResponse().getHeaders().add("X-Permitted-Cross-Domain-Policies", "none");
+//		        exchange.getResponse().getHeaders().add("Expect-CT", "max-age=86400, enforce");
+//		        exchange.getResponse().getHeaders().add("Access-Control-Allow-Methods", "POST, GET, OPTIONS,PATCH,PUT,DELETE");
 				
 			}));
 		};
