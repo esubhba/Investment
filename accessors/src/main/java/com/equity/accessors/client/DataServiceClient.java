0fcdcc42daaae4838eb7effb5691fdec6b7b0ef8
SU20126531 <subhadeep.baladhikari@wipro.com>
2021-06-28 19:46:21 +0530

    Add FallBack

--- accessors/src/main/java/com/equity/accessors/client/DataServiceClient.java
+++ accessors/src/main/java/com/equity/accessors/client/DataServiceClient.java
@@ -18,7 +18,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.equity.accessors.client.config.DataServiceFallBackFactory;
 import com.equity.accessors.data.model.ShareModel;
 
-@FeignClient(contextId = "EquityClient",name= "DATA-SERVICE",fallbackFactory = DataServiceFallBackFactory.class)
+@FeignClient(contextId = "EquityClient",name= "DATA-SERVICE",
+fallbackFactory = DataServiceFallBackFactory.class)
 public interface DataServiceClient {
 
 	@RequestMapping(path="/equity/{userId}/shares",consumes = {MediaTypes.HAL_JSON_VALUE},
